916b2e86ca1143c2b62ae761e524bbd47eef5999
Parth Bhanushali <116676891+The-Parth@users.noreply.github.com>
2024-01-20 16:23:30 +0530

    Added Creation of classroom class

--- Classes.py
+++ Classes.py
@@ -1,5 +1,13 @@
 import pandas as pd
 
+class Classrooms:
+    def __init__(self, building, room, capacity):
+        self.building = building
+        self.room = room
+        self.capacity = capacity
+
+    def __str__(self):
+        return f"Classrooms({self.building}, {self.room}, {self.capacity})"
 
 def classes_xlsx_to_dict(path = None):
     if path is None or path == "":
@@ -20,6 +28,26 @@ def classes_xlsx_to_dict(path = None):
     except Exception as e:
         print(e)
         return None
+    
+def get_classrooms(path: str = None):
+    if path is None or path == "":
+        raise Exception("Path Not Provided")
+    try:
+        df = pd.read_excel(path, sheet_name=None, header=0, index_col=0)
+        classrooms = []
+        for sheet in df:
+            # make sure the index column is read as string
+            df[sheet].index = df[sheet].index.astype(str)
+            for index, row in df[sheet].iterrows():
+                classrooms.append(Classrooms(sheet, index, row['Capacity']))
+        return classrooms
+    
+    except Exception as e:
+        print(e)
+        return None
 
 if __name__ == '__main__':
     print(classes_xlsx_to_dict("Classes.xlsx"))
+    classes = get_classrooms("Classes.xlsx")
+    for classroom in classes:
+        print(classroom)
